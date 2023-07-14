f35836e9283b9ef8cb17ca63921731f45a7d0731
Luizedu10s <112592262+Luizedu10s@users.noreply.github.com>
2023-07-14 14:56:00 -0300

    Commit 1.0

---
+++ cotaçãoDólar.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# REQUISIÇÃO NO SERVIDOR
+requisicao_server = requests.get("https://wise.com/br/currency-converter/dolar-hoje")
+# MOSTRAR CONTEÚDO DA PÁGINA
+pag = requisicao_server.content
+# TRANSFORMAR ATRAVÉS DO BEAUTIFULSOP, POIS PARA UTILIZAR AS FERRAMENTAS DA BIBILIOTECA É NECESSÁRIO FAZER ESTA CONVERSÃO.
+pag_html = BeautifulSoup(pag, "html.parser")
+# BUSCANDO INFORMAÇÕES NA PÁGINA
+cotacao = pag_html.find('h3', attrs={'class': 'cc__source-to-target'})
+# ENTRANDO NO INPUT 
+valor_cot = cotacao.find('span', attrs={'class': 'text-success'})
+# ARMAZENANDO EM UMA VARIÁVEL EM FORMA DE TEXTO.
+valor_dolar_final = valor_cot.text
+# CONVERTENDO PARA FLOAT.
+valor_float = float(valor_dolar_final)
+# SAÍDA DE DADOS NO PRINT
+print(f'\033[1;41;32mU$ 1 dólar equivale a R${valor_float:.2f} no dia de hoje.\033[m')
---
+++ mercado_livre.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL BASE PARA PODER BUSCAR NO SITE DO MERCADO LIVRE:
+url_base = 'https://lista.mercadolivre.com.br/'
+# INPUT PARA SABER O PRODUTO QUE O USUÁRIO QUER BUSCAR NO SITE:
+nome_produto = input("Digite o nome do produto que você quer buscar: ")
+# REQUISIÇÃO AO SITE, É SÓ CONCATENAR A URL BASE JUNTO AO NOME DO PRODUTO DIGITADONO INPUT.
+requisicao = requests.get(url_base + nome_produto)
+# BUSCA POR PRODUTO ESPECÍFICO NO SITE.
+pag_html = BeautifulSoup(requisicao.content, 'html.parser')
+# BUSCAR DENTRO DO HTML
+pag = pag_html.find('li', attrs={'class': 'ui-search-layout__item'})
+# NOME DO PRODUTO DENTRO DO SITE
+nome_no_site = pag.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'}).text
+# PREÇO DO PRODUTO DENTRO DO SITE.
+preco_produto = float(pag.find('span', attrs={'class': 'andes-money-amount__fraction'}).text)
+print(f'\033[1;30;47m O produto {nome_no_site} custa: R${preco_produto} \033[m ')
